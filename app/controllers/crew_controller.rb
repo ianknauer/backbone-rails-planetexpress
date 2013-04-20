35674145cc1748bad9a02068bcaf82c04564508b
Brian Mann <brian@osysit.com>
2013-04-20 04:53:00 -0400

    added crew model validations + implementation for create/update actions

--- app/controllers/crew_controller.rb
+++ app/controllers/crew_controller.rb
@@ -10,4 +10,25 @@ class CrewController < ApplicationController
 		# sleep 5
 		@member = Crew.find params[:id]
 	end
+	
+	def update
+		sleep 2
+		@member = Crew.find params[:id]
+		if @member.update_attributes params
+			render "crew/show"
+		else
+			respond_with @member
+		end
+	end
+	
+	def create
+		sleep 2
+		@member = Crew.new
+		if @member.update_attributes params
+			render "crew/show"
+		else
+			respond_with @member
+		end
+	end
+	
 end
--- app/models/crew.rb
+++ app/models/crew.rb
@@ -1,3 +1,6 @@
 class Crew < ActiveRecord::Base
   attr_accessible :age, :avatar, :name, :origin, :quote, :species, :title
+
+	validates_presence_of :name
+	validates_presence_of :title, :origin, :age, :avatar, :species, :unless => proc { new_record? }
 end
